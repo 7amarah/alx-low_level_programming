97a73631031227b3bf4d897e7bfa0ea3a655baf2
Tamarah Waritimi <kaemi-c@outlook.com>
2022-10-30 17:39:40 -0700

    correction to the task 7 solution

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,14 +10,13 @@
 
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
 
 	if (n < 0)
 	{
 		n = n * -1;
 	}
 
-	_putchar(last_digit + '0');
+	_putchar('0' + (n % 10));
 
-	return (last_digit);
+	return (n % 10);
 }
